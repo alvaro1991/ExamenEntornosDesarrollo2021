3d4d0a5ccdaaea5c3d9d8743cdbc244854abe415
Álvaro <amauri@sonoc.io>
2021-05-20 19:51:14 +0200

    Tercera clase añadida mediante feature

---
+++ src/main/java/Menu.java
@@ -0,0 +1,48 @@
+public class Menu {
+
+    private String nombre;
+    private String descripcion;
+    private String foto;
+    private String direccion;
+    private boolean disponible;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+}
