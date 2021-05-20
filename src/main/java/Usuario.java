db2226d45d5964c35e86b2449464a7f1811ecd03
Álvaro <amauri@sonoc.io>
2021-05-20 19:40:21 +0200

    Primer commit de primera clase Usuario

---
+++ src/main/java/Usuario.java
@@ -0,0 +1,39 @@
+public class Usuario {
+
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private String contraseña;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+}
